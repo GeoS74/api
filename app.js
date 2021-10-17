41caefa549dbc31fe854d469dc906cedfbe47b73
GeoS74 <gsirotkin@yandex.ru>
2021-10-17 19:37:28 +0500

    socket стабильно открывается и всё...

--- app.js
+++ app.js
@@ -37,17 +37,89 @@ app.use(async (ctx, next) => {
 
 const router = new Router();
 
+
+const {v4: uuidv4} = require('uuid');
+const pug = require('pug');
+router.get('/test', async ctx => {
+    //console.log(require('uuid').v5());
+    //ctx.body = uuidv4();
+    // const err = new Error({'goo':'bingo'});
+    // err.name = 'ValidationError';
+    // ctx.throw(408, err);
+
+    ctx.body = pug.renderFile(
+        path.join(__dirname, './templates/form') + '.pug'
+    );
+});
+
+
+/*
+const Koa = require("koa");
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+
+const app = new Koa();
+const httpServer = createServer(app.callback());
+const io = new Server(httpServer, {  options  });
+*/
+
+// const { createServer } = require("http");
+// const { Server } = require("socket.io");
+
+// const httpServer = createServer(app.callback());
+// const io = new Server(app, {
+//         cors: {
+//           origin: "http://localhost:3000",
+//           methods: ["GET", "POST"]
+//         }
+//       });
+
+// // Создание сервера
+const httpServer = require('http').createServer();
+// Берём API socket.io
+const io = require('socket.io')(httpServer, {
+    cors: {
+      origin: "http://localhost:3000",
+      methods: ["GET", "POST"]
+    }
+  });
+ 
+// Подключаем клиенты
+io.on('connection', () => {
+    // Выводим в консоль 'connection'
+    console.log('connection')
+    // Отправляем всем кто подключился сообщение привет
+    io.emit('hello', 'Привет')
+    // Что делать при случае дисконнекта
+    io.on('disconnect', () => {
+        // Выводи 'disconnected'
+        console.log('disconnected');
+    });
+});
+
+io.on('message', message => {
+    console.log('message', message);
+});
+ 
+// Назначаем порт для сервера
+httpServer.listen(3001);
+
+router.get('/chat', async (ctx, next) => {
+    ctx.set('content-type', 'text/html');
+    ctx.body = await fs.createReadStream(path.join(__dirname, './templates/form_chat.html'));
+});
+router.post('/chat', async (ctx, next) => {
+    
+    ctx.body = 'ok';
+});
+
+
+
+
 router.get('/user', allUsers);
 router.get('/user/:id', userById);
 router.post('/user', createUser);
 router.post('/user/:id', updateUser);
-
-const {v4: uuidv4} = require('uuid');
-router.get('/test', ctx => {
-    //console.log(require('uuid').v5());
-    ctx.body = uuidv4();
-});
-
 
 app.use(router.routes());
 
